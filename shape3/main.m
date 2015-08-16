6592a97e369dbdd97914f2087f8ae6b1967e8c5c
Elise Harris <eliseharris@Elises-MacBook-Air.local>
2015-08-16 15:29:16 -0400

    Initial Commit

---
+++ shape3/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  shape3
+//
+//  Created by Elise Harris on 8/16/15.
+//  Copyright (c) 2015 Elise Harris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
